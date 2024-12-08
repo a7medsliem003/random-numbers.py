e1a65b16728d97e7ff558601d441c1ae7e45a0e3
ahmed ayman <133252964+a7medsliem003@users.noreply.github.com>
2024-12-09 00:07:01 +0200

    Add files via upload

---
+++ Ahmed Ayman team3 (2).py
@@ -0,0 +1,12 @@
+from math import *
+list=[33,66,9,76,43,89]
+print(max(list))
+print(min(list))
+for k in (list):
+ if k % 2 == 0:
+    print("even")
+ else:
+    print("odd")
+result1= float(list[1])*float(list[2])*float(list[3])*float(list[4])*float(list[5])
+result2=float(list[0])*float(result1)
+print(result2)
